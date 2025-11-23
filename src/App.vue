669da76d7590fad2a655adbc18448d3e98180563
Joshua Flores <joshuaflores@mac.mynetworksettings.com>
2025-11-23 11:22:51 -0500

    added header

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,25 @@ import Button from 'primevue/button'
 </script>
 
 <template>
-  <div class="text-7xl bg-purple-900 min-h-screen flex flex-col">
-    <nav class="flex items-center justify-center gap-4 w-full py-2 border-b-2 border-orange-400">
-      <Button text>HOME</Button><Button text>SCHEDULE</Button><Button text>ROSTER</Button
-      ><Button text>EVENTS</Button>
+  <div class="flex min-h-screen flex-col bg-purple-900 text-7xl">
+    <nav
+      class="flex w-full items-center justify-center gap-4 border-b-2 border-orange-400 py-2"
+    >
+      <Button text>HOME</Button><Button text>SCHEDULE</Button
+      ><Button text>ROSTER</Button><Button text>EVENTS</Button>
     </nav>
+    <header class="relative max-h-120 w-full overflow-hidden">
+      <video
+        src="/public/video/hero-background.mp4"
+        autoplay
+        muted
+        loop
+        class="h-full w-full object-cover lg:object-[center_-10rem] xl:object-[center_-20rem]"
+      />
+      <img
+        src="/public/logo.svg"
+        class="absolute top-1/2 left-1/2 h-4/5 -translate-x-2/5 -translate-y-1/2 drop-shadow-xl drop-shadow-orange-300 filter"
+      />
+    </header>
   </div>
 </template>
