f81dfe111beb93cb1dfe28a845837b3fa35acbac
Joshua Flores <joshuaflores@mac.mynetworksettings.com>
2025-11-23 10:26:45 -0500

    added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from 'primevue/button'
+</script>
 
 <template>
-  <div class="text-7xl bg-purple-900">
-    <h1>SLASHING PUMPKINS</h1>
+  <div class="text-7xl bg-purple-900 min-h-screen flex flex-col">
+    <nav class="flex items-center justify-center gap-4 w-full py-2 border-b-2 border-orange-400">
+      <Button text>HOME</Button><Button text>SCHEDULE</Button><Button text>ROSTER</Button
+      ><Button text>EVENTS</Button>
+    </nav>
   </div>
 </template>
