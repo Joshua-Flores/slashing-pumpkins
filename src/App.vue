e0de97828c3a8ce2b5f8ec1b0fb048028969635d
Joshua Flores <joshuaflores@Joshuas-MacBook-Air.local>
2025-12-18 06:51:51 -0600

    initial layout of upcoming games

--- src/App.vue
+++ src/App.vue
@@ -9,51 +9,72 @@ import Button from 'primevue/button'
     >
       <Button size="large" text class="font-bold">HOME</Button
       ><Button size="large" text class="font-bold">SCHEDULE</Button
-      ><Button size="large" text class="font-bold">ROSTER</Button
-      ><Button size="large" text class="font-bold">EVENTS</Button>
+      ><Button size="large" text class="font-bold">STATS</Button>
     </nav>
-    <div class="space-y-8">
-      <header
-        class="relative max-h-120 w-full overflow-hidden border-b-4 border-orange-400"
-      >
-        <h1 class="sr-only">Slashing Pumpkins Home</h1>
-        <video
-          src="/public/video/hero-background.mp4"
-          autoplay
-          muted
-          loop
-          class="h-full w-full object-cover lg:object-[center_-10rem] xl:object-[center_-20rem]"
-          aria-label="hockey players warming up on a rink"
-        />
-        <img
-          src="/public/logo.svg"
-          alt="slashing pumpkin team logo"
-          class="absolute top-1/2 left-1/2 h-4/5 -translate-x-2/5 -translate-y-1/2 drop-shadow-sm drop-shadow-orange-300 filter"
-        />
-      </header>
-      <main class="container mx-auto grid px-6 xl:grid-cols-3">
-        <section>
-          <h2 class="mb-4 text-center text-4xl font-bold text-white">
-            UPCOMING GAMES
-          </h2>
+    <header
+      class="relative max-h-120 w-full overflow-hidden border-b-4 border-orange-400"
+    >
+      <h1 class="sr-only">Slashing Pumpkins Home</h1>
+      <video
+        src="/public/video/hero-background.mp4"
+        autoplay
+        muted
+        loop
+        class="h-full w-full object-cover lg:object-[center_-10rem] xl:object-[center_-20rem]"
+        aria-label="hockey players warming up on a rink"
+      />
+      <img
+        src="/public/logo.svg"
+        alt="slashing pumpkin team logo"
+        class="absolute top-1/2 left-1/2 h-4/5 -translate-x-2/5 -translate-y-1/2 drop-shadow-sm drop-shadow-orange-300 filter"
+      />
+    </header>
+    <main class="container mx-auto p-8">
+      <section>
+        <h2 class="mb-4 text-center text-2xl font-bold text-white md:text-4xl">
+          UPCOMING GAMES
+        </h2>
+
+        <div class="clip-corners bg-orange-400 p-0.5">
           <div
-            class="flex gap-4 rounded-md bg-linear-to-r from-purple-600 to-purple-700 px-4 py-2 text-white"
+            class="clip-corners grid grid-cols-5 bg-[url(/public/hockey-ice.png)] bg-center"
           >
-            <img
-              src="/public/hockey-puck.svg"
-              alt="hockey puck"
-              class="w-10 fill-amber-400 filter"
-            />
-            <div>
-              <p class="text-xl font-extrabold">
-                DEC 12
-                <span class="text-orange-400">vs</span> Mezazoic Puck
-              </p>
-              <p>9:40PM - Fairfax Ice Arena</p>
+            <!-- Versus -->
+            <div
+              class="col-span-3 flex items-center gap-3 p-3 text-white md:gap-6 md:px-6"
+            >
+              <img
+                src="/public/logo.svg"
+                alt="slashing pumpkins"
+                class="h-10 md:h-12"
+              />
+              <img
+                src="/public/lightning-bolt.svg"
+                alt="lightning-bolt"
+                class="mt-1 h-9 drop-shadow-lg drop-shadow-orange-300 md:mt-2 md:h-12"
+              />
+              <span class="leading-snug font-extrabold uppercase md:text-2xl"
+                >MEGALOSAURUS</span
+              >
+            </div>
+            <!-- Time -->
+            <div
+              class="col-span-2 flex flex-col items-center justify-center gap-1 p-3 md:gap-2"
+            >
+              <div
+                class="flex items-center gap-2 text-sm leading-none font-extrabold text-white md:gap-3 md:text-xl"
+              >
+                <span>DEC 21</span>
+                <span class="h-full w-0.5 bg-orange-300" />
+                <span>10:41 PM</span>
+              </div>
+              <span class="text-sm leading-none text-purple-300 md:text-lg"
+                >FAIRFAX ICE ARENA</span
+              >
             </div>
           </div>
-        </section>
-      </main>
-    </div>
+        </div>
+      </section>
+    </main>
   </div>
 </template>
