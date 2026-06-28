255395f7089c8d079def52d2e340a11c539049f2
Josh Flores <joshua.flores86@gmail.com>
2026-06-28 10:56:40 -0400

    updated schedule

--- src/data/games.ts
+++ src/data/games.ts
@@ -1,6 +1,6 @@
 // All times are Eastern Time (America/New_York)
 // EST (UTC-5) Nov–Mar 8; EDT (UTC-4) Mar 8–Nov
-// Year inferred from schedule: Dec 2025 – Jun 2026
+// Year inferred from schedule: Jun 2026 – Aug 2026
 
 export type Game = {
   awayTeam: string
@@ -11,155 +11,49 @@ export type Game = {
 }
 
 export const games: Game[] = [
-  // December 2025
-  {
-    awayTeam: 'Slashing Pumpkins',
-    homeTeam: 'Red Wings',
-    time: '2025-12-07T21:45:00-05:00',
-    awayTeamScore: 7,
-    homeTeamScore: 3,
-  },
-  {
-    awayTeam: 'Roccos',
-    homeTeam: 'Slashing Pumpkins',
-    time: '2025-12-14T23:00:00-05:00',
-    awayTeamScore: 2,
-    homeTeamScore: 2,
-  },
-  {
-    awayTeam: 'Slashing Pumpkins',
-    homeTeam: 'Hershey Beers',
-    time: '2025-12-21T22:40:00-05:00',
-    awayTeamScore: 2,
-    homeTeamScore: 1,
-  },
-
-  // January 2026
-  {
-    awayTeam: 'Blue Dogs',
-    homeTeam: 'Slashing Pumpkins',
-    time: '2026-01-04T20:45:00-05:00',
-    awayTeamScore: 2,
-    homeTeamScore: 5,
-  },
-  {
-    awayTeam: 'Slashing Pumpkins',
-    homeTeam: 'Megalosaurus',
-    time: '2026-01-11T20:55:00-05:00',
-    awayTeamScore: 1,
-    homeTeamScore: 5,
-  },
-  {
-    awayTeam: 'Red Wings',
-    homeTeam: 'Slashing Pumpkins',
-    time: '2026-01-30T23:55:00-05:00',
-    awayTeamScore: 3,
-    homeTeamScore: 7,
-  },
-
-  // February 2026
-  {
-    awayTeam: 'Slashing Pumpkins',
-    homeTeam: 'Roccos',
-    time: '2026-02-14T22:40:00-05:00',
-    awayTeamScore: 7,
-    homeTeamScore: 1,
-  },
-  {
-    awayTeam: 'Slashing Pumpkins',
-    homeTeam: 'Blue Dogs',
-    time: '2026-02-22T16:55:00-05:00',
-    awayTeamScore: 4,
-    homeTeamScore: 3,
-  },
-
-  // March 2026
-  {
-    awayTeam: 'Hershey Beers',
-    homeTeam: 'Slashing Pumpkins',
-    time: '2026-03-01T19:45:00-05:00',
-    awayTeamScore: 1,
-    homeTeamScore: 7,
-  },
-  {
-    awayTeam: 'Slashing Pumpkins',
-    homeTeam: 'Thunder',
-    time: '2026-03-15T21:30:00-04:00',
-    awayTeamScore: 2,
-    homeTeamScore: 2,
-  },
-  {
-    awayTeam: 'Blue Dogs',
-    homeTeam: 'Slashing Pumpkins',
-    time: '2026-03-28T23:55:00-04:00',
-    awayTeamScore: 4,
-    homeTeamScore: 2,
-  },
-
-  // April 2026
-  {
-    awayTeam: 'Slashing Pumpkins',
-    homeTeam: 'Megalosaurus',
-    time: '2026-04-03T23:30:00-04:00',
-    awayTeamScore: 2,
-    homeTeamScore: 1,
-  },
-  {
-    awayTeam: 'Slashing Pumpkins',
-    homeTeam: 'Red Wings',
-    time: '2026-04-11T22:10:00-04:00',
-    awayTeamScore: 5,
-    homeTeamScore: 3,
-  },
-  {
-    awayTeam: 'Thunder',
-    homeTeam: 'Slashing Pumpkins',
-    time: '2026-04-12T22:50:00-04:00',
-    awayTeamScore: 3,
-    homeTeamScore: 0,
-  },
-  {
-    awayTeam: 'Roccos',
-    homeTeam: 'Slashing Pumpkins',
-    time: '2026-04-19T21:40:00-04:00',
-    awayTeamScore: 0,
-    homeTeamScore: 6,
-  },
-  {
-    awayTeam: 'Slashing Pumpkins',
-    homeTeam: 'Hershey Beers',
-    time: '2026-04-25T23:30:00-04:00',
-    awayTeamScore: 5,
-    homeTeamScore: 3,
-  },
-
-  // May 2026
-  {
-    awayTeam: 'Thunder',
-    homeTeam: 'Slashing Pumpkins',
-    time: '2026-05-10T20:10:00-04:00',
-    awayTeamScore: 5,
-    homeTeamScore: 0,
-  },
-  {
-    awayTeam: 'Red Wings',
-    homeTeam: 'Slashing Pumpkins',
-    time: '2026-05-24T20:10:00-04:00',
-    awayTeamScore: 1,
-    homeTeamScore: 7,
-  },
-  {
-    awayTeam: 'Slashing Pumpkins',
-    homeTeam: 'Roccos',
-    time: '2026-05-31T22:50:00-04:00',
-    awayTeamScore: 7,
-    homeTeamScore: 2,
-  },
-
   // June 2026
   {
-    awayTeam: 'Megalosaurus',
+    awayTeam: 'Cretaceous Puck',
     homeTeam: 'Slashing Pumpkins',
-    time: '2026-06-07T09:30:00-04:00',
+    time: '2026-06-29T22:40:00-04:00',
+  },
+
+  // July 2026
+  {
+    awayTeam: 'Slashing Pumpkins',
+    homeTeam: 'Lightning',
+    time: '2026-07-12T22:50:00-04:00',
+  },
+  {
+    awayTeam: 'Junkyard Dogs',
+    homeTeam: 'Slashing Pumpkins',
+    time: '2026-07-20T20:00:00-04:00',
+  },
+  {
+    awayTeam: 'Slashing Pumpkins',
+    homeTeam: 'Alers',
+    time: '2026-07-27T20:00:00-04:00',
+  },
+
+  // August 2026
+  {
+    awayTeam: 'Bottle Caps',
+    homeTeam: 'Slashing Pumpkins',
+    time: '2026-08-08T08:20:00-04:00',
+  },
+  {
+    awayTeam: 'Slashing Pumpkins',
+    homeTeam: 'Cretaceous Puck',
+    time: '2026-08-17T21:40:00-04:00',
+  },
+  {
+    awayTeam: 'Lightning',
+    homeTeam: 'Slashing Pumpkins',
+    time: '2026-08-23T09:40:00-04:00',
+  },
+  {
+    awayTeam: 'Slashing Pumpkins',
+    homeTeam: 'Junkyard Dogs',
+    time: '2026-08-31T19:00:00-04:00',
   },
 ]
